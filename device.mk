bc63cd01aad3e310ed666653e2656c8d04fb4bf1
dianlujitao <dianlujitao@lineageos.org>
2022-02-15 14:43:26 +0800

    nairo: Enable fingerprint navigation
    Change-Id: Ic007af32c8e54caa278d4f973b5fc7a63e5f90aa

--- device.mk
+++ device.mk
@@ -56,6 +56,12 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1.vendor
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/idc/uinput-egis.idc:$(TARGET_COPY_OUT_VENDOR)/usr/idc/uinput-egis.idc \
+    $(LOCAL_PATH)/idc/uinput-fpc.idc:$(TARGET_COPY_OUT_VENDOR)/usr/idc/uinput-fpc.idc \
+    $(LOCAL_PATH)/keylayout/uinput-egis.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-egis.kl \
+    $(LOCAL_PATH)/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-fpc.kl
+
 # FM
 PRODUCT_PACKAGES += \
     FM2 \

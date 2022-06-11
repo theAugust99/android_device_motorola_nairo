bd1aa26b0a65a1cd1c970d472c264bfa9ffe70fb
theAugust99 <Gutobarroszk@gmail.com>
2022-06-11 17:19:56 +0000

    Bringup to corvus

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_nairo.mk
+    $(LOCAL_DIR)/corvus_nairo.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_nairo-eng \
-    lineage_nairo-user \
-    lineage_nairo-userdebug
+    corvus_nairo-eng \
+    corvus_nairo-user \
+    corvus_nairo-userdebug

01491e532de604734a1ed484248bbed987ab6508
theAugust99 <Gutobarroszk@gmail.com>
2022-07-26 03:11:00 +0000

    Bringup to spark

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_nairo.mk
+    $(LOCAL_DIR)/spark_nairo.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_nairo-eng \
-    lineage_nairo-user \
-    lineage_nairo-userdebug
+    spark_nairo-eng \
+    spark_nairo-user \
+    spark_nairo-userdebug
